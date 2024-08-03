05c4bdf2e3631a2012b69a1124d7c7eefd5444c3
bbl <bbl@host118.MAIN.MFVSEGEI.RU>
2024-08-03 15:32:40 +0300

    HW #42 commit #1

--- src/telran/numbers/test/GroupsSumTest.java
+++ src/telran/numbers/test/GroupsSumTest.java
@@ -2,17 +2,23 @@ package telran.numbers.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 import telran.numbers.*;
 class GroupsSumTest {
-private static final int N_GROUPS = 1000;
+private static final int N_GROUPS = 100000;
 private static final int GROUP_LENGTH = 1000;
 int[][] groups = {
 		{1,2},
 		{3,4},
 		{5,6}
 };
-int[][] largeGroups = getLargeGroups(N_GROUPS, GROUP_LENGTH);
+
 	@Test
 	void goupsSumTaskThreadTest() {
 		GroupsSum gs = new GroupsSumTaskThread(groups);
@@ -21,7 +27,65 @@ int[][] largeGroups = getLargeGroups(N_GROUPS, GROUP_LENGTH);
 	private int[][] getLargeGroups(int nGroups, int groupLength) {
 		// TODO creating random two dimensional array 
 		// using method generate of Stream
-		return null;
+		int[][] largeGroup=new int[nGroups][groupLength];
+		Random rand = new Random();
+		Supplier<int[]> sup = () -> rand.ints(groupLength, Integer.MIN_VALUE, Integer.MAX_VALUE).toArray();
+		largeGroup = Stream.generate(sup).limit(nGroups).toArray(int[][]::new);
+		return largeGroup;
+	}
+	
+	@Test
+	void getLargeGroupTest()
+	{
+		int nY=10;
+		int nX=5;
+		int[][] largeGroup=getLargeGroups(nY,nX);
+		for(int i=0;i<nY;i++)
+		{
+			for(int j=0;j<nX;j++)
+			{
+				System.out.printf("%12d, ",largeGroup[i][j]);
+			}
+			System.out.printf("\n");
+		}
+	}
+	
+	@Test
+	void performanceTest()
+	{
+		printTitle();
+		int[][] largeGroup;
+		for(int i=10;i<=N_GROUPS;i=i*10)
+		{
+			largeGroup=getLargeGroups(i,GROUP_LENGTH);
+			long tTaskThread = getTimeWorkTaskThread(largeGroup);
+			long tThreadPool = getTimeWorkThreadPoll(largeGroup);
+			String strAbout=String.format("[%d %d] ", i,GROUP_LENGTH);
+			System.out.printf("%20s %20d %20d\n",strAbout,tTaskThread,tThreadPool);
+		}
+	}
+	
+	private void printTitle() 
+	{
+		System.out.printf("%20s %20s %20s\n", "Dimension_of_Array","Time_of_TaskThread","Time_of_ThreadPool");
+	}
+	
+	private long getTimeWorkThreadPoll(int[][] largeGroup)
+	{
+		Instant start = Instant.now();
+		groupsSumTaskThreadPerformance(largeGroup);
+		Instant finish = Instant.now();
+		long t= Duration.between(start, finish).toMillis();
+		return t;
+	}
+	
+	private long getTimeWorkTaskThread(int[][] largeGroup) 
+	{
+		Instant start = Instant.now();
+		groupsSumTaskThreadPerformance(largeGroup);
+		Instant finish = Instant.now();
+		long t= Duration.between(start, finish).toMillis();
+		return t;
 	}
 	@Test
 	void goupsSumThreadPoolTest() {
@@ -29,11 +93,13 @@ int[][] largeGroups = getLargeGroups(N_GROUPS, GROUP_LENGTH);
 		assertEquals(21, gs.getSum());
 	}
 	@Test
-	void groupsSumTaskThreadPerformance() {
+	void groupsSumTaskThreadPerformance(int[][] largeGroups) 
+	{
 		new GroupsSumTaskThread(largeGroups).getSum();
 	}
 	@Test
-	void groupsSumTaskThreadPoolsPerformance() {
+	void groupsSumTaskThreadPoolsPerformance(int[][] largeGroups)
+	{
 		new GroupsSumThreadPool(largeGroups).getSum();
 	}
 
